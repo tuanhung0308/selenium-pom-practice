00f728e6e6cae819167ed801d6fe40e76c4e41cb
tuanhung0308 <vthung03@gmail.com>
2026-04-16 10:02:28 +0700

    integrate CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,31 @@
-from email.policy import default
-import webbrowser
 import os
+import webbrowser
 import pytest
 from selenium import webdriver
 
-def pytest_sessionfinish(session, exitstatus):
 
-    html_flag_path = session.config.getoption("htmlpath", default=None)
+def pytest_sessionfinish(session, exitstatus):
+    html_flag_path = getattr(session.config.option, "htmlpath", None)
     if html_flag_path:
         report_path = os.path.abspath(html_flag_path)
         file_url = f"file:///{report_path}"
-        print("\n[+] Will auto open report file: {html_flag_path}")
+        print(f"\n[+] Will auto open report file: {html_flag_path}")
         webbrowser.open(file_url)
+
 
 @pytest.fixture
 def browser(request):
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    headless = os.getenv("HEADLESS", "true").lower() in ("1", "true", "yes")
+    if headless:
+        options.add_argument("--headless=new")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-extensions")
+
+    driver = webdriver.Chrome(options=options)
     driver.maximize_window()
     driver.implicitly_wait(10)
     request.node.driver = driver
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,23 +1,19 @@
 import time
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def test_login_demo():
-    driver = webdriver.Chrome()
-    driver.get("https://www.saucedemo.com/")
+def test_login_demo(browser):
+    browser.get("https://www.saucedemo.com/")
 
-    usernamefield = driver.find_element(By.XPATH, "//input[@id='user-name']")
+    usernamefield = browser.find_element(By.XPATH, "//input[@id='user-name']")
     usernamefield.send_keys("standard_user")
-    wait = WebDriverWait(driver, 10)
-    
+    wait = WebDriverWait(browser, 10)
+
     passwordfield = wait.until(EC.presence_of_element_located((By.XPATH, "//input[@id='password']")))
     passwordfield.send_keys("secret_sauce")
-    wait = WebDriverWait(driver, 10)
 
-    loginbutton = driver.find_element(By.XPATH, "//input[@id='login-button']")
+    loginbutton = browser.find_element(By.XPATH, "//input[@id='login-button']")
     loginbutton.click()
 
     time.sleep(5)
-    driver.quit()
